5cef8e8631ff646076205ab8de5403ccf8e66e90
Ankesh Chopde <acchopde84@gmail.com>
2023-04-09 19:09:32 +0530

    backend

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const connection = require("./configs/db");
+const userRoutes = require("./routes/user.routes");
+const cors = require('cors')
+require("dotenv").config();
+const app = express();
+app.use(cors())
+app.use(express.json());
+app.use("/user",userRoutes)
+
+
+
+app.get("/",(req,res)=>{
+    res.send("Home Page")
+});
+
+
+app.listen(process.env.port,async()=>{
+  try{
+    await connection
+    console.log("connected to database")
+  }
+  catch{
+    console.log("error while connecting to database")
+  }
+  console.log("connected to server")
+})
