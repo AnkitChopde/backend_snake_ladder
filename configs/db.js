7b7dd50c19a9add8a80eabae38ab8eac186af06e
Ankesh Chopde <acchopde84@gmail.com>
2023-04-10 22:37:25 +0530

    push

--- configs/db.js
+++ configs/db.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
 require('dotenv').config()
-const connection = mongoose.connect(process.env.mongoURL,
-    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+const connection = mongoose.connect(process.env.mongoURL);
 
 module.exports = connection;
